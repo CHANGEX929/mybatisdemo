076edd35898d02048e7c34227f6d5268137ae717
xiecx@carelinker.com <xiecx@carelinker.com>
2018-09-03 10:22:57 +0800

    Initial commit

---
+++ src/main/java/pojo/User.java
@@ -0,0 +1,44 @@
+package pojo;
+
+/**
+ * @Author Xie Chenxi
+ * @Date 2018/8/31 12:24
+ */
+public class User {
+    private int id;
+    private String name;
+    private int age;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
